b9e8b0868170011cd2abd70493894682a75ce0fe
NiciusB <nuno@balbona.me>
2018-12-15 04:31:01 +0100

    Improved code and improved race conditions on memory writes

--- index.js
+++ index.js
@@ -33,41 +33,44 @@ config.shards.forEach(shard => {
     notYetPushedRooms: {}
   }
 })
+var writeAfterNextRead = false
 
-async function readMemorySegment () {
-  config.shards.forEach(shard => {
-    api.segment.get(config.segment, shard).then(memory => {
-      if (!memory.ok) console.error(JSON.stringify(memory))
-      if (memory.data === null || memory.data === '') {
-        console.log(`Welcome! Make sure to use the in-game API to request room data`)
-        api.segment.set(config.segment, JSON.stringify({
-          lastUpdated: 0,
-          requests: [],
-          responses: {}
-        }), shard)
-        return null
-      }
-      try {
-        shards[shard].data = JSON.parse(memory.data)
-        if (typeof shards[shard].data !== 'object' || !Array.isArray(shards[shard].data.requests)) throw new Error('') // Fire catch block
-      } catch (e) {
-        console.error(`The segment does not have valid data, make sure that it's not being used elsewhere. The current data is: ${memory.data}`)
-        process.exit()
-      }
+async function readMemorySegment (shard, doWrite) {
+  const memory = await api.segment.get(config.segment, shard)
+  if (!memory.ok) console.error(JSON.stringify(memory))
+  if (memory.data === null || memory.data === '') {
+    console.log(`Welcome! Make sure to use the in-game API to request room data`)
+    api.segment.set(config.segment, JSON.stringify({
+      lastUpdated: 0,
+      requests: [],
+      responses: {}
+    }), shard)
+    return null
+  }
+  try {
+    shards[shard].data = JSON.parse(memory.data)
+    if (typeof shards[shard].data !== 'object' || !Array.isArray(shards[shard].data.requests)) throw new Error('') // Fire catch block
+  } catch (e) {
+    console.error(`The segment on ${shard} does not have valid data, make sure that it's not being used elsewhere. The current data is: ${memory.data}`)
+    process.exit()
+  }
 
-      processRequests(shard)
-    }).catch(err => console.error(err))
-  })
+  await processRequests(shard)
+  if (doWrite) writeMemorySegment(shard)
 }
 
-function processRequests (shard) {
+async function processRequests (shard) {
   if (!api.socket.connected) return false // Socket died on us. Wait for reconnect
 
   shards[shard].data.requests = shards[shard].data.requests.filter(reqRoom => shards[shard].notYetPushedRooms[reqRoom] === undefined)
-  shards[shard].data.requests.forEach(async reqRoom => {
+  const toAwait = []
+  shards[shard].data.requests.forEach(reqRoom => {
     shards[shard].notYetPushedRooms[reqRoom] = true
-    shards[shard].notYetPushedRooms[reqRoom] = await getRoomData(`room:${shard}/${reqRoom}`)
+    toAwait.push(getRoomData(`room:${shard}/${reqRoom}`).then(result => {
+      shards[shard].notYetPushedRooms[reqRoom] = result
+    }))
   })
+  await Promise.all(toAwait)
 }
 
 function getRoomData (path) {
@@ -81,26 +84,34 @@ function getRoomData (path) {
   })
 }
 
-function writeMemorySegment () {
-  config.shards.forEach(shard => {
-    var changed = false
-    for (let roomID in shards[shard].notYetPushedRooms) {
-      if (shards[shard].notYetPushedRooms[roomID] === true) continue
-      changed = true
-      shards[shard].data.responses[roomID] = shards[shard].notYetPushedRooms[roomID]
-      delete shards[shard].notYetPushedRooms[roomID]
-      const indexToRemove = shards[shard].data.requests.indexOf(roomID)
-      if (indexToRemove !== -1) shards[shard].data.requests.splice(indexToRemove, 1)
-    }
-    if (!changed) return false // Nothing to push
-    shards[shard].data.lastUpdated = Date.now() / 1000
-    api.segment.set(config.segment, JSON.stringify(shards[shard].data), shard)
-  })
+function writeMemorySegment (shard) {
+  var changed = false
+  for (let roomID in shards[shard].notYetPushedRooms) {
+    if (shards[shard].notYetPushedRooms[roomID] === true) continue
+    changed = true
+    shards[shard].data.responses[roomID] = shards[shard].notYetPushedRooms[roomID]
+    delete shards[shard].notYetPushedRooms[roomID]
+    const indexToRemove = shards[shard].data.requests.indexOf(roomID)
+    if (indexToRemove !== -1) shards[shard].data.requests.splice(indexToRemove, 1)
+  }
+  if (!changed) return false // Nothing to push
+  shards[shard].data.lastUpdated = Date.now() / 1000
+  api.segment.set(config.segment, JSON.stringify(shards[shard].data), shard)
 }
 
-console.log(`Screeps Free Observer started on segment ${config.segment}!`)
+function doReads () {
+  config.shards.forEach(shard => {
+    readMemorySegment(shard, writeAfterNextRead)
+  })
+  writeAfterNextRead = false
+}
+
 const readsPerSec = (rateLimits.memorySegmentRead / 60 / 60) / config.shards.length * 0.95 // 0.95 is just margin for errors
 const writesPerSec = (rateLimits.memorySegmentWrite / 60 / 60) / config.shards.length * 0.95 // 0.95 is just margin for errors
-setInterval(readMemorySegment, 1000 / readsPerSec)
-setInterval(writeMemorySegment, 1000 / writesPerSec)
-readMemorySegment()
+setInterval(doReads, 1000 / readsPerSec)
+setInterval(() => {
+  writeAfterNextRead = true
+}, 1000 / writesPerSec)
+
+console.log(`Screeps Free Observer started on segment ${config.segment}!`)
+doReads()
